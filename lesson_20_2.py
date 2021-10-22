5e4e8ed2da928e2c9d95606ca20deba1c952154a
Maksim Pecherytsia <findirmk@gmail.com>
2021-10-22 13:21:51 +0300

    +

--- lesson_20_2.py
+++ lesson_20_2.py
@@ -1,3 +1,4 @@
+# 1 task
 # def add_num(seq, number):
 #     seq = list(seq)
 #     for i_num in range(len(seq)):
@@ -11,6 +12,7 @@
 # print(origin_tuple)
 # print(changed_list)
 
+# 2 task
 # import random
 #
 #
@@ -24,26 +26,47 @@
 # third_tuple = first_tuple + second_tuple
 #
 # print(third_tuple, 'нолей =', third_tuple.count(0))
-import math
+
+# 3 task
+# import math
+#
+#
+# def square_calculation(r, h):
+#     '''
+#     Функция для расчёта площади боковой поверхности цилиндра и его полной площади.
+#     :param r: радиус
+#     :param h: высота
+#     :return: возвращает side - боковую поверхность и full - полную площадь.
+#     '''
+#     side = 2 * math.pi * r * h
+#     s = math.pi * r**2
+#     full = side + 2 * s
+#     return round(side,2 ), round(full, 2)
+#
+#
+# r = int(input('Input radius: '))
+# h = int(input('Input height: '))
+#
+# side, full = square_calculation(r, h)
+#
+# print('\nПлощадь боковой поверхности =', side)
+# print('Полная площадь =', full)
+
+# 4 task
+import random
 
 
-def square_calculation(r, h):
-    '''
-    Функция для расчёта площади боковой поверхности цилиндра и его полной площади.
-    :param r: радиус
-    :param h: высота
-    :return: возвращает side - боковую поверхность и full - полную площадь.
-    '''
-    side = 2 * math.pi * r * h
-    s = math.pi * r**2
-    full = side + 2 * s
-    return round(side,2 ), round(full, 2)
+def change(nums):
+    nums  = list(nums)
+    index = random.randint(0, 5)
+    value = random.randint(100, 1000)
+    nums[index] = value
+    return tuple(nums), value
 
 
-r = int(input('Input radius: '))
-h = int(input('Input height: '))
+my_nums = 1, 2, 3, 4, 5
 
-side, full = square_calculation(r, h)
-
-print('\nПлощадь боковой поверхности =', side)
-print('Полная площадь =', full)
+new_nums, rand_val = change(my_nums)
+new_nums_2, rand_val_2 = change(my_nums)
+print(new_nums, rand_val)
+print(new_nums_2, rand_val + rand_val_2)
